314715fe5f33da6562d768ebb70f7c2c590d47c8
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-05-16 02:42:30 +0300

    Fix Path

--- Test/test.py
+++ Test/test.py
@@ -3,7 +3,7 @@ import shutil
 import sqlite3
 import pytest
 from fastapi.testclient import TestClient
-from yolo.app import app, UPLOAD_DIR, PREDICTED_DIR, DB_PATH, init_db
+from YoloServiceFursa.app import app, UPLOAD_DIR, PREDICTED_DIR, DB_PATH, init_db
 
 client = TestClient(app)
 
